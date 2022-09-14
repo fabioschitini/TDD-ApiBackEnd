90d145ab13737728ad0b7dd98e52f4b5515ef928
Fabio Schitini <fabioschitini1@hotmail.com>
2022-09-14 09:59:18 -0300

    .

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors=require("cors")
-
+const flash = require('express-flash')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const session = require("express-session");
@@ -42,6 +42,9 @@ app.use(session({
       secure:true
   },
 }));
+
+app.use(flash())
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
