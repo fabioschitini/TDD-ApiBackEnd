00aac136670276780ac380357674aa775ef51cca
Fabio Schitini <fabioschitini1@hotmail.com>
2022-09-14 10:04:29 -0300

    .

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const gamesController=require('../controllers/gameController')
 
 /* GET home page. */
 router.get('/',(req,res,next)=>{
-  res.json('Welcome to my TDD API3000')
+  res.json('Welcome to my TDD APIalll')
 });
 
 router.post('/signin',userController.signin_post);
